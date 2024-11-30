df87f400daa762de83f97f735c88a0fca1ecb816
riyaroy2501 <riii.95.rr@gmail.com>
2024-11-30 21:23:15 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -32,8 +32,7 @@ if ingredients_List:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
-        
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
 # st.write(my_insert_stmt)
