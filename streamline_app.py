08c58dff88670fa5237c3b94f50ac36e5438e5aa
riyaroy2501 <riii.95.rr@gmail.com>
2024-11-30 20:41:05 +0530

    Update streamline_app.py
    connection changes needed because of moving from SiS to SiS

--- streamline_app.py
+++ streamline_app.py
@@ -9,7 +9,9 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!"""
 )
 
-session = get_active_session()
+cnx = st.connection ("snowflake")
+session = cnx.session()
+# session = get_active_session()
 # my_dataframe = session.table("smoothies.public.fruit_options").select (col ('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
